fe4bc42091d995208cec0656ebc6f9a37cb13579
oscarb102 <oscarb102@gmail.com>
2024-11-26 23:46:25 -0500

    Add getPoints logic

--- src/main/java/com/fetch/receipt_processor/utils/PointsUtils.java
+++ src/main/java/com/fetch/receipt_processor/utils/PointsUtils.java
@@ -1,5 +1,10 @@
 package com.fetch.receipt_processor.utils;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import com.fetch.receipt_processor.entity.Item;
 import com.fetch.receipt_processor.entity.Receipt;
 
 public class PointsUtils {
@@ -10,6 +15,9 @@ public class PointsUtils {
         res += roundDollar(receipt.getTotal());
         res += totalIsMultiple(receipt.getTotal(), .25);
         res += everyTwoItems(receipt.getItems().size());
+        res += trimmedItemDescriptions(receipt.getItems());
+        res += purchaseDayIsOdd(receipt.getPurchaseDate());
+        res += timeOfPurchase(receipt.getPurchaseTime());
 
         return res;
     }
@@ -38,5 +46,35 @@ public class PointsUtils {
 
     private static int everyTwoItems(int size) {
         return (size / 2) * 5;
+    }
+
+    private static int trimmedItemDescriptions(List<Item> items) {
+        int res = 0;
+
+        for (Item item: items) {
+            String trimmedItemDesc = item.getShortDescription().trim();
+            if (trimmedItemDesc.length() % 3 == 0) {
+                double price = Double.parseDouble(item.getPrice());
+                res += (int) Math.ceil(price * .2);
+            }
+        }
+
+        return res;
+    }
+
+    private static int purchaseDayIsOdd(String date) {
+        LocalDate parsedDate = LocalDate.parse(date);
+        int day = parsedDate.getDayOfMonth();
+
+        return (day % 2 != 0) ? 6 : 0;
+    }
+
+    private static int timeOfPurchase(String time) {
+        LocalTime twoPm = LocalTime.of(14, 0, 0);
+        LocalTime fourPm = LocalTime.of(16, 0,  0);
+        LocalTime parsedTime = LocalTime.parse(time);
+        boolean inTimeRange = (parsedTime.isAfter(twoPm) && parsedTime.isBefore(fourPm));
+
+        return (inTimeRange) ? 10 : 0;
     }
 }
