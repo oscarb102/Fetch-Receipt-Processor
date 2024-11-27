b7356b0bd5ac61ca8cf99a2344cb740f230cb9b8
oscarb102 <oscarb102@gmail.com>
2024-11-26 22:42:29 -0500

    Add response for successful receipt addition

--- src/main/java/com/fetch/receipt_processor/controller/receipt/ReceiptController.java
+++ src/main/java/com/fetch/receipt_processor/controller/receipt/ReceiptController.java
@@ -1,5 +1,7 @@
 package com.fetch.receipt_processor.controller.receipt;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fetch.receipt_processor.service.ReceiptManager;
 import com.fetch.receipt_processor.utils.IdGeneratorUtils;
 
+import java.util.HashMap;
 import java.util.Map;
 import com.fetch.receipt_processor.entity.Receipt;
 
@@ -25,12 +28,15 @@ public class ReceiptController {
 
     @PostMapping
     @RequestMapping("receipts/process")
-    public void addReceipt(@RequestBody Receipt receipt) {
+    public ResponseEntity<Map<String, String>> addReceipt(@RequestBody Receipt receipt) {
         ReceiptManager receiptManager = ReceiptManager.getInstance();
-        
+
         String id = IdGeneratorUtils.createId();
 
         receiptManager.addReceipt(id, receipt);
+        Map<String, String> response = new HashMap<>();
+        response.put("id", id);
+        return new ResponseEntity<Map<String, String>>(response, HttpStatus.CREATED);
     }
 
 }
